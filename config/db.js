510bf5411001e5795c25d09484c3766685f9a465
devGospel <ozchange2002@gmail.com>
2024-08-23 10:57:02 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 const connectDB = async () =>{
-    mongoose.connect(process.env.MONGODB_URI, 
+    mongoose.connect(process.env.MONGODB_URI  , 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
